df7fe1e7d7f56695f6b5b0cefea97255bcde9b3f
Kou <hikkappi@yahoo.co.jp>
2026-01-02 22:17:30 +0900

    Add: 認証済みユーザー情報取得用のメソッドをUserControllerに追加

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -14,6 +15,29 @@ class UserController extends Controller
     {
         // Laravelが自動的にIDからUserを取得してくれる
         // 見つからない場合は自動で404を返す
+        return new UserResource($user);
+    }
+
+    /**
+     * 認証済みユーザー自身の情報を返す
+     */
+    public function me(Request $request): UserResource
+    {
+        // $request->user() で認証済みユーザーを取得
+        return new UserResource($request->user());
+    }
+
+    public function put(Request $request): UserResource
+    {
+        // 認証済みユーザーのみが更新可能
+        $user = $request->user();
+
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+        ]);
+
+        $user->update($validated);
+
         return new UserResource($user);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,9 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user/{user}', [App\Http\Controllers\Api\UserController::class, 'show']);
+
+// 認証必要（要ログイン）
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [App\Http\Controllers\Api\UserController::class, 'me']);
+    Route::put('/me', [App\Http\Controllers\Api\UserController::class, 'put']);
+});
